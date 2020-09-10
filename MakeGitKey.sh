1414ae2ecd20e91b55f04014c0eb676060637d3e
ToneDef00 <daniels@dual4t.com>
2020-09-09 17:45:04 -0700

    added spelling fix

--- MakeGitKey.sh
+++ MakeGitKey.sh
@@ -2,5 +2,5 @@ ssh-keygen -t rsa -b 4096 -C "daniels@dual4t.com"
 eval "$(ssh-agent -s)"
 ssh-add /Users/$USER/.ssh/id_rsa
 git config --global user.name "ToneDef00"
-git config --gloabl user.email "daniels@dual4t.com"
+git config --global user.email "daniels@dual4t.com"
 cat ~/.ssh/id_rsa.pub
