19cfe85b17ba2ed8db9f50ab0f226a7b89f715e7
ToneDef00 <daniels@dual4t.com>
2020-09-09 19:46:47 -0700

    added flower watcher limit support as well as updating the environmental variables so the PATH includes SNAP

--- ubuntuInitialSetup.sh
+++ ubuntuInitialSetup.sh
@@ -3,6 +3,13 @@ sudo apt install mysql-server -y
 sudo apt-get install mysql-workbench
 sudo apt-get install curl
 sudo apt-get install build-essential
-sudo apt-get install snap
+sudo rm /etc/apt/preferences.d/nosnap.pref
+sudo apt updateC
+sudo apt install snapd
+SNAPPATH=:/snap
+echo "PATH=$PATH$CPATH" > /etc/environment
+source /etc/engironment
 sudo snap install --classic code
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
