ce56675ecc852de74f75b71be27a53029561b7ed
Daniel Steigman <35793213+ToneDef00@users.noreply.github.com>
2020-09-11 20:53:11 -0700

    Update MakeGitKey.sh

--- MakeGitKey.sh
+++ MakeGitKey.sh
@@ -1,6 +1,6 @@
 ssh-keygen -t rsa -b 4096 -C "daniels@dual4t.com"
 eval "$(ssh-agent -s)"
-ssh-add /Users/$USER/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 git config --global user.name "ToneDef00"
 git config --global user.email "daniels@dual4t.com"
 cat ~/.ssh/id_rsa.pub
