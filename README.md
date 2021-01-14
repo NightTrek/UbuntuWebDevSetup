04394939f718b36f37fe423c167088303b612220
ToneDef00 <daniels@dual4t.com>
2021-01-13 19:12:54 -0800

    added better functionality to go and protobuf script

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ First Step is to Run the main UbuntuSetup.sh file
 then you run the keyGen file to get the github ssh key.
 Then you need to set up nvm by changing the directories permissions with " sudo chmod -R 777 .nvm/ " in your home directory
 Then you need to finish setting up mysql so it uses a password preferably with the password of "password"
+
+installing protobuff link
+https://askubuntu.com/questions/532701/how-can-i-install-protobuf-in-ubuntu-12-04
+some protobuff error links
+https://stackoverflow.com/questions/25518701/protobuf-cannot-find-shared-libraries
--- installNugebaseGo.sh
+++ installNugebaseGo.sh
@@ -1,6 +1,18 @@
+echo "installing go version 1.14.12 amd64 linux"
 cd /tmp
-wget https://dl.google.com/go/go1.11.linux-amd64.tar.gz
-sudo tar -xvf go1.11.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.14.12.linux-amd64.tar.gz
+sudo tar -xvf go1.14.12.linux-amd64.tar.gz
 sudo mv go /usr/local
 GOROOT=/usr/local/go
 GOPATH=$HOME/go
+
+echo 'installing protobuf 3.14 from github'
+
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/protobuf-all-3.14.0.tar.gz
+sudo tar -xvf protobuf-all-3.14.0.tar.gz
+cd protobuf-3.14.0/
+./configure
+make
+make check
+sudo make install
+protoc --version
